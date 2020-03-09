4a6fe81ef7d814f72c191fbd92694efccd2072ca
Stefan Djordjevic <stefan_djordjevic1991@hotmail.com>
2020-03-09 17:06:51 +0100

    Added dampening(on ground) and jumpcut

--- Da Sanjam/Assets/_Scripts/PlayerMovement.cs
+++ Da Sanjam/Assets/_Scripts/PlayerMovement.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    public float jumpVel;
+    [SerializeField]
+    [Range(0, 10)]
+    float jumpVel = 8f;
+
+    [SerializeField]
+    [Range(0, 5)]
+    float gravExtra = 2.5f;
 
     private bool grounded;
-    private bool jumped = false;
 
-    public float gravExtra;
-    
+    float groundedRemember = 0;
+    [SerializeField]
+    float groundedRememberTime = .25f;
+
+    float jumpPressedRemember = 0;
+    [SerializeField]
+    float jumpPressedRememberTime = .2f;
+
+    [SerializeField]
+    [Range(0, 1)]
+    float cutJumpHeight = 0.5f;
+    bool jumpButtonUp = false;
+
     private float Xdir;
 
     [SerializeField]
-    LayerMask lmGround;
+    float xAcc = 1;
+    [SerializeField]
+    [Range(0, 1)]
+    float xVelDampBasic = 0.5f;
+    [SerializeField]
+    [Range(0, 1)]
+    float xVelDampWhenStopping = 0.5f;
+    [SerializeField]
+    [Range(0, 1)]
+    float xVelDampWhenTurning = 0.5f;
+
+    [SerializeField]
+    private LayerMask lmGround;
     
     Rigidbody2D rb;
 
@@ -26,34 +53,95 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        Xdir = Input.GetAxisRaw("Horizontal") * speed;
+        
 
-        if(Input.GetButtonDown("Jump") && grounded)
+        CheckIfGrounded();
+        
+        //Get movement speed and direction
+        Xdir = Input.GetAxisRaw("Horizontal");
+
+        //checks and remembers if player was grounded
+        groundedRemember -= Time.deltaTime;
+        if (grounded)
         {
-            jumped = true;
+            groundedRemember = groundedRememberTime;
+            jumpButtonUp = false;
+        }
+
+        //checks and remembers if a button gets pressed
+        jumpPressedRemember -= Time.deltaTime;
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumpPressedRemember = jumpPressedRememberTime;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpButtonUp = true;
         }
     }
 
     private void FixedUpdate()
+    {
+        //TODO: add dampening to x when grounded
+        //      add dampening to x when not grounded
+
+        CalculateMovement(Xdir);
+
+        CalculateJumping(groundedRemember, jumpPressedRemember);
+
+
+        //falling a little faster
+        if (rb.velocity.y < 0)
+        {
+            Vector2 fasterFall = (Vector2.up * (Physics2D.gravity.y * gravExtra)) * Time.deltaTime;
+            rb.velocity += fasterFall;
+        }
+    }
+
+    private void CheckIfGrounded()
     {
         //Using overlapbox to check if the player hit the ground. I'm using a layer mask
         //to filter out the rest of the objects in the scene
         Vector2 groundedBoxCheckPos = (Vector2)transform.position + new Vector2(0, -0.01f);
         Vector2 groundedBoxSize = (Vector2)transform.localScale + new Vector2(-0.02f, 0);
         grounded = Physics2D.OverlapBox(groundedBoxCheckPos, groundedBoxSize, 0, lmGround);
+    }
 
-        rb.velocity = new Vector2(Xdir, rb.velocity.y);
-
-        //TODO: add timer for more responsiveness
-        if (jumped)
+    private void CalculateJumping(float ground, float jumpPress)
+    {
+        if ((ground > 0) && (jumpPress > 0))
         {
+            groundedRemember = 0;
+            jumpPressedRemember = 0;
             rb.velocity = new Vector2(rb.velocity.x, jumpVel);
-            jumped = false;
         }
 
-        if(rb.velocity.y < 0)
+        if(jumpButtonUp)
         {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * gravExtra * Time.deltaTime;
+            if (rb.velocity.y > 0)
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * cutJumpHeight);
         }
+    }
+
+    private void CalculateMovement(float mXdir)
+    {
+        float xVel = rb.velocity.x;
+        xVel += Xdir;
+
+        Debug.Log(rb.velocity.x);
+
+        if (Mathf.Abs(Xdir) < 0.01f)
+            xVel *= Mathf.Pow(1f - xVelDampWhenStopping, Time.deltaTime * 10f);      
+        else if (Mathf.Sign(Input.GetAxisRaw("Horizontal")) != Mathf.Sign(xVel))
+            xVel *= Mathf.Pow(1f - xVelDampWhenTurning, Time.deltaTime * 10f);
+        else
+            xVel *= Mathf.Pow(1f - xVelDampBasic, Time.deltaTime * 10f);
+
+        if ((xVel < 0.001f) && (xVel > -0.001f))
+            xVel = 0;
+
+        rb.velocity = new Vector2(xVel, rb.velocity.y);
+
     }
 }
